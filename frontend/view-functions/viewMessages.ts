83c427c03f6f470f18297f204398f92b995e43a0
JamesKm08 <jmkamau08@gmail.com>
2024-10-08 21:24:52 +0300

    feat:view entry fun

--- frontend/view-functions/viewMessages.ts
+++ frontend/view-functions/viewMessages.ts
@@ -2,26 +2,21 @@ import { aptosClient } from "@/utils/aptosClient";
 import { useWallet } from "@aptos-labs/wallet-adapter-react";
 import { useState, useEffect } from "react";
 
-const MODULE_ADDRESS = "0xc17bf48d3bd22cc4a1dbb432445d2701d4c18dd7adda12852c250e130a81b648";
-const MODULE_NAME = "billboard";
+const MODULE_ADDRESS = "0xef42aa6a2dad0b6d2aeb58624efb9defead0bc59c80bba9c4e965a2b6288196e";
+const MODULE_NAME = "mindful";
 
-export const viewMessages = async () => {
-    if (!account) return [];
+export const viewMessages = async (): Promise<string> => {
+  const content = await aptosClient()
+    .view<[string]>({
+      payload: {
+        function: `${MODULE_ADDRESS}::${MODULE_NAME}::get_messages`,
+      },
+    })
+    .catch((error) => {
+      console.error(error);
+      return ["Messages do not exist"];
+    });
 
-    try {
-        const response = await aptosClient().getAccountResource(
-               `${MODULE_ADDRESS}::${MODULE_NAME}::Billboard`
-             );
-
-        const { data } = response as any;
-        return data.messages.map((msg: any) => ({
-           sender: msg.sender,
-           message: msg.message,
-           added_at: new Date(msg.added_at * 1000),
-           }));
-        } catch (error) {
-            console.error("Failed to fetch messages", error);
-            return [];
-           }
+  return content[0];
 };
 
