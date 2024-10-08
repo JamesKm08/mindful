bdc70c65f384c8de7171c91ca8f3ed34bd6b48c3
JamesKm08 <jmkamau08@gmail.com>
2024-10-08 21:22:35 +0300

    fix:write message funtionality

--- frontend/entry-functions/writeMessage.ts
+++ frontend/entry-functions/writeMessage.ts
@@ -1,7 +1,7 @@
 import { InputTransactionData } from "@aptos-labs/wallet-adapter-react";
 
-const MODULE_ADDRESS = "0xc17bf48d3bd22cc4a1dbb432445d2701d4c18dd7adda12852c250e130a81b648"
-const MODULE_NAME = "billboard"
+const MODULE_ADDRESS = "0xef42aa6a2dad0b6d2aeb58624efb9defead0bc59c80bba9c4e965a2b6288196e"
+const MODULE_NAME = "mindful"
 
 export type WriteMessageArguments = {
   content: string; // the content of the message
