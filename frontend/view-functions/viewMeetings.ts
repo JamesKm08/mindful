da747783b40855b855e77373ebad07c23cd28e7f
JamesKm08 <jmkamau08@gmail.com>
2024-10-10 22:05:11 +0300

    fix: fetch meeting links

--- frontend/view-functions/viewMeetings.ts
+++ frontend/view-functions/viewMeetings.ts
@@ -1,31 +1,22 @@
-import { MODULE_ADDRESS } from "@/constants";
 import { aptosClient } from "@/utils/aptosClient";
 
 const MODULE_ADDRESS = "0xef42aa6a2dad0b6d2aeb58624efb9defead0bc59c80bba9c4e965a2b6288196e"
 const MODULE_NAME = "mindful"
 
-interface MeetingLinks {
-  alcoholAnon: string;
-  gamblerAnon: string;
-}
+export const getMeetingLinks = async (): Promise<{ alcoholAnon: string; gamblerAnon: string }> => {
+      const response = await aptosClient()
+      .view<[String, String]>({
+          payload: {
+              function: `${MODULE_ADDRESS}::mindful::get_meeting_links`,
+          },
+      })
+  .catch((error) => {
+      console.error(error);
+      return { alcoholAnon: "", gamblerAnon: "" };
+      });
 
-export const getMeetingLinks = async (): Promise<MeetingLinks> => {
-  try {
-    const response = await aptosClient().view({
-      function: `${MODULE_ADDRESS}::mindful::get_meeting_links`,
-      type_arguments: [],
-      arguments: [],
-    });
-
-    return {
+  return {
       alcoholAnon: response[0] as string,
-      gamblerAnon: response[1]as string,
-    };
-  } catch (error) {
-    console.error("Error fetching meeting links:", error);
-    return {
-      alcoholAnon: "",
-      gamblerAnon: "",
-    };
-  }
+      gamblerAnon: response[1] as string,
+      };
 };
