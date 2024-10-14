8dc2ef091ac30ffe1a52a114cde90f681f1229f8
JamesKm08 <jmkamau08@gmail.com>
2024-10-15 02:16:58 +0300

    fix fetch msgs

--- frontend/view-functions/viewMessages.ts
+++ frontend/view-functions/viewMessages.ts
@@ -1,22 +1,19 @@
 import { aptosClient } from "@/utils/aptosClient";
-import { useWallet } from "@aptos-labs/wallet-adapter-react";
-import { useState, useEffect } from "react";
 
-const MODULE_ADDRESS = "0xef42aa6a2dad0b6d2aeb58624efb9defead0bc59c80bba9c4e965a2b6288196e";
+const MODULE_ADDRESS = "0x4493314e5bde4dad14a902a126973cac90fa26e1b4921aeb6645cb647eee8176";
 const MODULE_NAME = "mindful";
 
-export const viewMessages = async (): Promise<string> => {
-  const content = await aptosClient()
-    .view<[string]>({
+export const viewMessages = async (): Promise<Array<{ sender: string; message: string; added_at: u64 }>> => {
+  const response = await aptosClient()
+    .view<[Array<{ sender: string; message: string; added_at: u64 }>]>({
       payload: {
-        function: `${MODULE_ADDRESS}::${MODULE_NAME}::get_messages`,
+        function: `${MODULE_ADDRESS}::mindful::get_messages`,
       },
     })
     .catch((error) => {
       console.error(error);
-      return ["Messages do not exist"];
+      return [];
     });
 
-  return content[0];
+  return response[0];
 };
-
